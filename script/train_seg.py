8bc9bb25c0b86e58455bbe53716bcb5542b74d98
yangjinhua <osmondy@foxmail.com>
2026-01-23 12:00:04 +0800

    优化脚本目录变量

--- script/train_seg.py
+++ script/train_seg.py
@@ -5,15 +5,16 @@ import os
 # 请将 '/workspace/my_booth_project' 替换为你希望保存所有训练产出的实际目录
 
 # 定义一个基础目录
-project_dir = '../models/train'
+project_dir = '/home/aistudio/YOLO/'
+train_output_path = project_dir + 'models/train/'
 # 为本次训练指定一个名称，训练文件会保存在以该名称命名的子文件夹中
 exp_name = 'booth_seg_v1'
 
-yolo_model="../models/yolo11/yolo11m-seg.pt"
+yolo_model = project_dir + "models/yolo11/yolo11m-seg.pt"
 # yolo_model="/workspace/models/yolo26/yolo26m-seg.pt"
 
 # 确保目录存在
-os.makedirs(project_dir, exist_ok=True)
+os.makedirs(train_output_path, exist_ok=True)
 
 # 2. 加载模型
 # 注意：你原代码加载的是yolo11n.pt，这里已修正
@@ -22,14 +23,14 @@ model = YOLO(yolo_model)  # load a pretrained model (recommended for training)
 # 3. 开始训练
 results = model.train(
     # data='coco8-seg.yaml',  # 数据集配置文件路径
-    data='./booth_seg.yaml',  # 数据集配置文件路径
+    data= project_dir + 'script/booth_seg.yaml',  # 数据集配置文件路径
     epochs=100,                               # 训练轮数
     imgsz=640,                                # 输入图像尺寸
     batch=-1,                                  # 批次大小，即每次训练迭代使用的样本数；-1： 自动计算最大可用 batch
     # augment=True,                             # 启用数据增强，以提高模型的泛化能力
     device=0,                                 # 训练设备
     # ================= 以下是新增/修改的关键参数 =================
-    project=project_dir,                      # 【新增】指定项目根目录
+    project=train_output_path,                      # 【新增】指定项目根目录
     name=exp_name,                            # 【新增】指定实验名称
     save=True,                                # 保存训练结果和模型（默认即为True）
     save_period=-1,                           # 每N轮保存一次检查点，-1表示仅在最后保存
